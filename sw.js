383efbc944433bba62b6d08e5df8a4e4f882ac63
mustaffapp <designingyourfabric@gmail.com>
2026-02-24 00:56:12 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'kushane-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './kushane-icon.png',
+  './pigeon_darkened.jpg',
+  './birdhouse_icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
